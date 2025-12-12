1b2b361166cae4b07cb9f123199bfef5dc60076c
BrownieByte <suchetkumbar@gmail.com>
2025-12-12 20:52:10 +0530

    Phase 1

---
+++ app/routes/home.tsx
@@ -0,0 +1,22 @@
+import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "CareerLens" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>
+}
