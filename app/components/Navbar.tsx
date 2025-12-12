31eb32ec48e37b17fc4eb3c6c8647d432a522079
BrownieByte <suchetkumbar@gmail.com>
2025-12-13 00:48:15 +0530

    Authentication

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <Link to='/'>
+        <p className="text-2xl font-bold text-gradient">CareerLens</p>
+      </Link>
+
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </nav>
   );
 };
 
